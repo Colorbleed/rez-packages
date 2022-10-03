367f233531000cdd4094c658e68967141999bb15
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 20:16:40 +0000

    Adding numeric_owner as keyword arguement

--- openexr/2.2.0/rezbuild.py
+++ openexr/2.2.0/rezbuild.py
@@ -39,7 +39,7 @@ def build(source_path, build_path, install_path, targets):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, build_path)
